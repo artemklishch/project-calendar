1c7cfb5c035e64c85c0c62d8c4f802c05a9bccb0
Artem <artemklishch@gmail.com>
2019-12-22 13:59:20 +0200

    rt

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,5 +2,5 @@ import { renderTimingSidebar } from './sidebar_timing.js';
 import { sidebarDays } from './sidebar_days.js';
 import { renderCurrentWeek } from './current_week.js';
 import { renderAnotherWeek } from './generate_another_week.js';
-//import { renderTitleDate } from './generate_title_date.js';
+import { renderTitleDate } from './generate_title_date.js';
 import { todayButtonFunc } from './today_button.js';
