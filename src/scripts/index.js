c155e125f0073b892e57ca34077d46efcc63c0c1
Artem <artemklishch@gmail.com>
2019-12-24 18:39:01 +0200

    еу

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,5 +4,5 @@ import { renderCurrentWeek } from './current_week.js';
 import { renderAnotherWeek } from './generate_another_week.js';
 import { renderTitleDate } from './generate_title_date.js';
 import { todayButtonFunc } from './today_button.js';
-import { renderRedLIne } from './redline.js';
+//import { renderRedLIne } from './redline.js';
 import { renderEventObject } from './generate_event_object.js';
