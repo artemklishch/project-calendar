d4a07a9f91c33954edb69a0a31dbcb4adad1f42f
Artem <artemklishch@gmail.com>
2019-12-24 13:46:03 +0200

    after func for generate events

--- src/scripts/storage.js
+++ src/scripts/storage.js
@@ -13,14 +13,32 @@ export const eventsArray = [
     },
     {
         header: 'Dinner with friends',
-        startTime: new Date(2019,11,24,18,0),
-        endTime: new Date(2019,11,24,19,30),
+        startTime: new Date(2019,11,29,18,0),
+        endTime: new Date(2019,11,29,19,30),
         description: undefined,
     },
     {
         header: 'Good night',
         startTime: new Date(2019,11,28,23),
         endTime: new Date(2019,11,28,23,45),
+        description: undefined,
+    },
+    {
+        header: 'nsxt',
+        startTime: new Date(2019,11,30,9),
+        endTime: new Date(2019,11,30,10),
+        description: undefined,
+    },
+    {
+        header: 'ыщьуерштп фе дфые',
+        startTime: new Date(2019,11,3,9),
+        endTime: new Date(2019,11,3,10),
+        description: undefined,
+    },
+    {
+        header: 'ыщьуерштп фе дфые',
+        startTime: new Date(2019,11,3,21),
+        endTime: new Date(2019,11,4,2),
         description: undefined,
     },
 ];
