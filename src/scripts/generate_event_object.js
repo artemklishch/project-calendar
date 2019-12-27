0da3e3409589b37a4ac94f713a2b30b91575f56a
Artem <artemklishch@gmail.com>
2019-12-27 19:14:38 +0200

    t

--- src/scripts/generate_event_object.js
+++ src/scripts/generate_event_object.js
@@ -58,6 +58,24 @@ const transformObjectFunc = (element) => {
     eventsArray.push(obj1, obj2);        
 };
 
+
+
+const transformHourFormat = (hour) => {
+    if(hour === 13) hour = 1;
+    if(hour === 14) hour = 2;
+    if(hour === 15) hour = 3;
+    if(hour === 16) hour = 4;
+    if(hour === 17) hour = 5;
+    if(hour === 18) hour = 6;
+    if(hour === 19) hour = 7;
+    if(hour === 20) hour = 8;
+    if(hour === 21) hour = 9;
+    if(hour === 22) hour = 10;
+    if(hour === 23) hour = 11;
+    if(hour === 24) hour = 0;
+    return hour;
+};
+
 const fillDayPlace = (dayObject) => {
     let certainHour = new Date(dayObject.startTime).getHours();
 
