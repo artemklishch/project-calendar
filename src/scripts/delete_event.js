a16b2cc6b251c106e19999bc391efb215166f694
Artem <artemklishch@gmail.com>
2019-12-27 09:14:01 +0200

    not all is done

---
+++ src/scripts/delete_event.js
@@ -0,0 +1,19 @@
+import { indexOfElement } from './edit_event.js';
+import { eventsArray } from './storage.js';
+import { renderEventObject, clearFunc } from './generate_event_object.js';
+import { renderRedLIne } from './redline.js';
+
+const deleteBasket = document.querySelector('.event__btn-delete');
+const popupBlock = document.querySelector('.popup-layer');
+const saveBtnForEdit = document.querySelector('.event__btn-save_after_edit');
+
+export const funcForDeleteEvene = () => {
+    eventsArray.splice(indexOfElement,1);
+
+    clearFunc();
+    renderEventObject(eventsArray);
+    popupBlock.style.display = 'none';
+    saveBtnForEdit.style.display = 'none';
+    renderRedLIne();
+};
+deleteBasket.addEventListener('click', funcForDeleteEvene);
