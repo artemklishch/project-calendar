338156f9a855581a489319e47cacb69dc85f53ef
Artem <artemklishch@gmail.com>
2020-01-26 23:05:27 +0200

    fix

--- src/scripts/generate_another_week.js
+++ src/scripts/generate_another_week.js
@@ -21,21 +21,14 @@ export const renderAnotherWeek = event => {
     if (certainArrow.classList.contains('nav__arow_right')) {
         arrDaysOfWeek
             .forEach(element => element.setDate(element.getDate() + 7));
-        let temp = new Date(arrDaysOfWeek[0]);
-        [...numbersOfDates]
-        .forEach(elem => {
-            let tempElem = elem.closest('.header__week-block_days');
-            tempElem.classList.remove('today__header__week-block_days');
-            elem.innerHTML = temp.getDate();
-            temp.setDate(temp.getDate() + 1);
-        });
         counter++;
-        renderEventObject();
     }
     if (certainArrow.classList.contains('nav__arow_left')) {
         arrDaysOfWeek
             .forEach(element => element.setDate(element.getDate() - 7));
-        let temp = new Date(arrDaysOfWeek[0]);
+        counter--;
+    }
+    let temp = new Date(arrDaysOfWeek[0]);
         [...numbersOfDates]
         .forEach(elem => {
             let tempElem = elem.closest('.header__week-block_days');
@@ -43,9 +36,7 @@ export const renderAnotherWeek = event => {
             elem.innerHTML = temp.getDate();
             temp.setDate(temp.getDate() + 1);
         });
-        counter--;
-        renderEventObject();
-    }
+    renderEventObject();
     renderTitleDate(arrDaysOfWeek[0], arrDaysOfWeek[6]);
     clearInterval(timerId);
     if (counter === 0) {
