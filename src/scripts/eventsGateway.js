d97f6eb675a3db4d006ef6932c7d32fe7f51e8a6
Artem <artemklishch@gmail.com>
2020-03-05 13:19:45 +0200

    new server 1

--- src/scripts/eventsGateway.js
+++ src/scripts/eventsGateway.js
@@ -1,4 +1,4 @@
-const baseUrl = 'https://crudcrud.com/api/4b57f02005404dec99d87785fea5c2a9/eventsArray';
+const baseUrl = 'https://crudcrud.com/api/73b38a8c21ab4fbf85b898b06a850bc0/eventsArray';
 
 const mapEvents = tasks => 
     tasks.map(({_id, ...rest}) => ({...rest, id: _id}));
